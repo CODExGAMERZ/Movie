0edb257136dc9110309b789ee6cbb3f8c763bfb9
Aryan <105963590+CODExGAMERZ@users.noreply.github.com>
2026-05-28 08:33:40 +0530

    Update and rename key.js.example to key.js

--- js/key.js
+++ js/key.js
@@ -1,3 +1,3 @@
 // Duplicate this file as key.js in the same folder and add your OMDB API key
 // Get your free API key at http://www.omdbapi.com/apikey.aspx
-const key = "YOUR_OMDB_API_KEY_HERE";
+const key = "7cb2ff62";
